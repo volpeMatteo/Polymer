9782cd90fc97e65af66537f16535fec50ec79220
volpeMatteo <32592981+volpeMatteo@users.noreply.github.com>
2018-10-30 08:32:19 +0100

    speranza

---
+++ src/Polymer-app/Polymer-app.js
@@ -0,0 +1,30 @@
+import { html, PolymerElement } from "../../node_modules/@polymer/polymer/polymer-element.js";
+import "../../node_modules/@polymer/iron-input/iron-input.js";
+
+class AppMessage extends PolymerElement {
+  static get template() {
+    return html`
+      <style>
+        :div {
+          align: center;
+        }
+      </style>
+     <div align="center">
+     Inserisci Nome: <iron-input bind-value="{{Nome}}"><input is="iron-input" placeholder="Insert Nome"></iron-input>
+     <h1>Polymer Saluta <b>{{Nome}}!!!</b> 👋</h1>
+     </div>
+    `;
+  }
+
+  static get properties() {
+    return {
+      Nome: {
+        type: String,
+        value: ''
+      }
+    };
+  }
+
+}
+
+window.customElements.define('app-message', AppMessage);
